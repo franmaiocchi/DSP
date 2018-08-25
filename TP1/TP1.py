66e038e03cd7c08c1dfd74df646106c6cb0d9305
F.Maiocchi <franmaiocchi@est.frba.utn.edu.ar>
2018-08-25 15:18:20 -0300

    Le agregue generador triangular

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -59,6 +59,22 @@ def generador_cuadrada (a0, N, d):
     
     signal = np.concatenate((a0*np.ones(int(np.round(N*d))),-a0*np.ones(int(np.round(N*(1-d))))))
     return signal
+
+def generador_triangular (amin, amax, N, d, fs):
+    
+    tt = np.linspace(0, (N-1)/fs, N)
+    Nsim = int(np.round(N*d))
+    
+    a = amin + (amax - amin)*tt[0:Nsim]/tt[Nsim]
+    b = (amin-amax)*tt[Nsim:N]/(tt[N-1] - tt[Nsim]) + amax - (amin-amax)*tt[Nsim]/(tt[N-1] - tt[Nsim])
+    
+    signal = np.concatenate((a,b))
+    return tt, signal
+             
+    
+    
+    signal = np.concatenate((a0*np.ones(int(np.round(N*d))),-a0*np.ones(int(np.round(N*(1-d))))))
+    return signal
 #%%  Testbench de senoidales    
 def testbench_senoidal ():
     
@@ -137,7 +153,31 @@ def testbench_cuadrada():
     N = 1000
     d = 0.22
     plt.plot(generador_cuadrada(1,N,d))
+    
+#%%  Testbench de triangular
+
+def testbench_triangular():
+    
+    N  = 1000 # muestras
+    fs = 1000 # Hz
+    
+    amin = -3
+    amax = 1
+    d = 0.2
+    
+    tiempo, señal = generador_triangular(amin, amax, N, d, fs)
+    plt.plot(tiempo, señal)
+    
+    # Label del ploteo   
+    plt.title('Señal: triangular')
+    plt.xlabel('Tiempo [segundos]')
+    plt.ylabel('Amplitud [V]')
+    plt.xlim((-0.05,1.05))
+    plt.ylim((1.05*amin,1.05*amax))
+
+    
 #%%  Seleccione el testbench que se quiere probar
 #testbench_senoidal()
 #testbench_ruido()
-testbench_cuadrada()
+#testbench_cuadrada()
+testbench_triangular()
